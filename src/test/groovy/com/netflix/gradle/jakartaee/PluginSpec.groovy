d0c52f15d70ecb0ca7981bae0325451e6be4a79d
Danny Thomas <dannyt@netflix.com>
2022-12-15 15:02:30 +1100

    Add test

---
+++ src/test/groovy/com/netflix/gradle/jakartaee/PluginSpec.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.gradle.jakartaee
+
+class PluginSpec extends AbstractPluginSpec {
+    def 'plugin applies when it is the only plugin applied'() {
+        expect:
+        runTasks('help')
+    }
+
+    def 'migration can be enabled when it is the only plugin applied'() {
+        // there are no configurations so we expect this to do nothing
+
+        buildFile << """
+jakartaeeMigration {
+    migrate()
+}
+"""
+
+        expect:
+        runTasks('help')
+    }
+
+    def 'enabling transform without the jvm ecosystem plugin does not fail'() {
+        buildFile << """
+jakartaeeMigration {
+    transform(configurations.create('myconfig'))
+}
+"""
+
+        expect:
+        runTasks('help')
+    }
+
+}
