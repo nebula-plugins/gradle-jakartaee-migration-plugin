4a104c634cb1a5c29d474643fe99932d7b0744c5
Danny Thomas <dannyt@netflix.com>
2022-12-16 12:44:13 +1100

    Add clarifying comment about why we're not applying attributes conditionally

--- src/main/kotlin/com/netflix/gradle/jakartaee/JakartaEeMigrationTransform.kt
+++ src/main/kotlin/com/netflix/gradle/jakartaee/JakartaEeMigrationTransform.kt
@@ -76,6 +76,13 @@ internal abstract class JakartaEeMigrationTransform : TransformAction<JakartaEeM
 
     override fun transform(outputs: TransformOutputs) {
         val inputFile = getInputArtifact().get().asFile
+        /*
+         * This is unfortunately a bit of a hack, but you can't use component metadata rules to apply attributes to
+         * artifacts. If you add them to the component, they *are* inherited by the artifact, but then the dependency
+         * will fail to resolve. Allowing it to resolve with compatibility rules then causes the transform not to run.
+         *
+         * There might be a way of doing this I'm missing, but this looks like the best I can do for now.
+         */
         if (excludedCachePaths.any { inputFile.path.contains(it) }) {
             LOGGER.debug("Skipping JakartaEE transform for {}", inputFile)
             outputs.file(inputFile)
