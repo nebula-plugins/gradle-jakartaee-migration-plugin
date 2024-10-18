4a9d850b291c8cd85494713f56c1d8f7a4e0949e
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2024-10-17 17:24:49 -0700

    Gradle 8.11-rc-1

--- src/test/groovy/com/netflix/gradle/jakartaee/CapabilityOnlySpec.groovy
+++ src/test/groovy/com/netflix/gradle/jakartaee/CapabilityOnlySpec.groovy
@@ -35,6 +35,6 @@ jakartaeeMigration {
 
         expect:
         def result = resolvedRuntimeClasspathFailureResult()
-        result.output.contains("Cannot select module with conflict on capability 'com.netflix.gradle.jakartaee:servlet-api:3.0.1' also provided by [javax.servlet:servlet-api:2.2(runtime), javax.servlet:servlet-api:2.2(runtime), jakarta.servlet:jakarta.servlet-api:4.0.2(runtime)]")
+        result.output.contains("Cannot select module with conflict on capability 'com.netflix.gradle.jakartaee:servlet-api:4.0.2' also provided by [javax.servlet:servlet-api:2.2(runtime), javax.servlet:javax.servlet-api:3.0.1(runtime)]")
     }
 }
